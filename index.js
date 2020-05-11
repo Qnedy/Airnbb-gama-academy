00afbccaba372a528b6da765387b36883e1a83f1
qnedy <altmaisefequatro@gmail.com>
2020-05-10 23:09:29 -0300

    finished

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const container = document.getElementsByClassName('container-cards');
 
+let pointers = [];
+
 async function getAPI(){
-  return await fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72")
-  .then(response => response.json());
+  return await fetch("https://my-json-server.typicode.com/Qnedy/fake-API-test/locations").then(response => response.json());
 }
 
 function createCardElement(card){
@@ -26,25 +27,49 @@ function createCardElement(card){
 }
 
 function innerCards(){
-  console.log(container);
   getAPI().then(response => response.forEach((card) => {
       let newCard = createCardElement(card);
       container[0].appendChild(newCard);
+      pointers.push({lat: card.Latitude, lng: card.Longitude});
   }));
+
+  carregarPontos();
 }
 
 innerCards();
 
-// content.map(item => {
-//   containerCards.innerHTML = `<div class="card-quarto">
-//   <img src="${item.photo}" >
-//   <div class="infos-quarto">
-//     <span>${item.property_type}</span>
-//     <h2>${item.name}</h2>
-//     <p>7 hóspedes - 1 quarto - 3 camas - 1 banheiro</p>
-//   </div>
-//   <div class="price">
-//     <p><span>R$ ${item.price}</span>/noite</p>
-//   </div>
-// </div>`
-// });
+function carregarPontos() {
+  pointers.map(ponto => {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(ponto.lat, ponto.lng),
+        title: "Meu ponto personalizado! :-D",
+        map: map
+    });
+  });
+
+}
+
+let time = setTimeout(function initMap() {
+  // The location of Uluru
+
+  var uluru = {lat: -19.212355602107472, lng: -44.20234468749999};var teste = {lat: -22.618827234831404, lng: -42.57636812499999};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  // var marker = new google.maps.Marker({position: uluru, map: map});
+
+  // var marker = new google.maps.Marker({position: teste, map: map});
+
+  pointers.map(ponto => {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(ponto.lat, ponto.lng),
+        title: "Meu ponto personalizado! :-D",
+        map: map
+    });
+  });
+
+  console.log(pointers);
+  clearTimeout(time);
+}, 2000)
+
